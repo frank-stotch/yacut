de386b3351b4a2a387fc89d5b6adb4bfc765ef7a
Frank Stotch <ya.inside38@gmail.com>
2025-01-29 14:22:55 +0500

    move some constants to forms

---
+++ yacut/forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
+
+from .models import MaxLength, Message, MinLength
+from .settings import SHORT_ID_PATTERN
+
+
+class Label:
+    LONG_URL = 'Длинная ссылка'
+    CUSTOM_SHORT_ID = 'Ваш вариант короткой ссылки'
+    SUBMIT = 'Создать'
+
+
+class URLMapForm(FlaskForm):
+    original_link = URLField(
+        label=Label.LONG_URL,
+        validators=[
+            DataRequired(Message.REQUIRED_FIELD),
+            URL(message=Message.INVALID_URL_PATTERN)
+        ]
+    )
+    custom_id = StringField(
+        label=Label.CUSTOM_SHORT_ID,
+        validators=[
+            Optional(),
+            Regexp(SHORT_ID_PATTERN, message=Message.INVALID_SHORT_ID_PATTERN),
+            Length(MinLength.SHORT_ID, MaxLength.SHORT_ID,
+                   Message.INVALID_SHORT_ID_LENGTH)
+        ]
+    )
+    submit = SubmitField(label=Label.SUBMIT)
