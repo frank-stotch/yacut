ff50daf557714595806d7710e9782346b2496c49
Frank Stotch <ya.inside38@gmail.com>
2025-01-29 14:23:17 +0500

    add short_id_pattern to settings

--- yacut/settings.py
+++ yacut/settings.py
@@ -1,6 +1,9 @@
 import os
 
 
+SHORT_ID_PATTERN = r'[A-Za-z0-9]+'
+
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY', 'MUMBAYUMBA')
