daa3bfe5f4d2144a885bd16d5f96e7d17f4710b0
Frank Stotch <ya.inside38@gmail.com>
2025-01-31 01:58:41 +0500

    fix views

--- yacut/views.py
+++ yacut/views.py
@@ -1,54 +1,35 @@
-from random import choices
+from http import HTTPStatus
 
-from flask import flash, redirect, render_template, url_for
-from werkzeug.exceptions import BadRequest
+from flask import abort, flash, redirect, render_template
 
 from . import app
 from .forms import URLMapForm
 from .models import URLMap
-from .settings import POSSIBLE_CHARACTERS, RANDOM_SHORT_ID_LENGTH
 
 
-SHORT_ID_READY = ('Ваша короткая ссылка готова: '
-                  '<a href="{short}">{short}</a>')
-SHORT_ALREADY_EXISTS = 'Предложенный вариант короткой ссылки уже существует.'
-
-
-def get_unique_short_id():
-    """
-    Генерирует уникальный короткий ID, который не существует в базе данных.
-    """
-    while True:
-        short_id = ''.join(choices(POSSIBLE_CHARACTERS,
-                           k=RANDOM_SHORT_ID_LENGTH))
-        if not URLMap.exists(short=short_id):
-            return short_id
-
-
-def get_unique_short_id_or_400(custom_short_id=None):
-    if not custom_short_id:
-        return get_unique_short_id()
-    if URLMap.exists(short=custom_short_id):
-        short_url = url_for(
-            'redirect_view', short_id=custom_short_id, _external=True)
-        raise BadRequest(SHORT_ALREADY_EXISTS.format(short=short_url))
-    return custom_short_id
+SHORT_ID_READY = ('Ваша короткая ссылка готова:')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLMapForm()
-    if form.validate_on_submit():
-        original = form.original_link.data
-        short_id = get_unique_short_id_or_400(form.custom_id.data)
-        entry = URLMap(original=original, short=short_id)
-        entry.save()
-        short_url = url_for('redirect_view', short_id=short_id, _external=True)
-        flash(SHORT_ID_READY.format(short=short_url), 'success')
-    return render_template('index.html', form=form)
+    if not form.validate_on_submit():
+        return render_template('index.html', form=form)
+    short = form.custom_id.data
+    try:
+        entry = URLMap.create(original=form.original_link.data,
+                              short=short)
+        return render_template('index.html',
+                               form=form,
+                               short=URLMap.get_url_for_short(entry.short))
+    except ValueError as e:
+        flash(str(e))
+        return render_template('index.html', form=form)
 
 
-@app.route('/<short_id:short_id>', methods=['GET'])
-def redirect_view(short_id):
-    entry = URLMap.query.filter_by(short=short_id).first_or_404()
-    return redirect(entry.original)
+@app.route('/<string:short>', methods=['GET'])
+def redirect_view(short: str):
+    original = URLMap.from_short(short=short)
+    if not original:
+        abort(HTTPStatus.NOT_FOUND)
+    redirect(original)
