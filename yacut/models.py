f75fb03f52ef7a50a0c632266db281fa377782c4
Frank Stotch <ya.inside38@gmail.com>
2025-01-31 01:57:31 +0500

    add micro-orm, del useless methods

--- yacut/models.py
+++ yacut/models.py
@@ -1,5 +1,8 @@
+import re
 from datetime import datetime
 from random import choices
+from typing import Union
+from urllib.parse import urlparse
 
 from flask import url_for
 
@@ -9,7 +12,8 @@ from .settings import (
     MAX_SHORT_LENGTH,
     POSSIBLE_CHARACTERS,
     RANDOM_SHORT_LENGTH,
-    REDIRECT_VIEW
+    REDIRECT_VIEW,
+    SHORT_PATTERN
 )
 
 
@@ -17,15 +21,21 @@ ORIGINAL_MAX_LENGTH = 256
 
 
 class Message:
-    SHORT_ALREADY_EXISTS = 'Вариант короткой ссылки "{}" уже существует'
     REQUIRED_FIELD = 'Обязательное поле'
+    SHORT_ALREADY_EXISTS = 'Вариант короткой ссылки "{}" уже существует'
     INVALID_URL_PATTERN = 'Неправильный формат ссылки'
-    INVALID_SHORT_ID_PATTERN = ('Можно использовать только'
-                                ' латинские буквы и цифры')
-    INVALID_SHORT_ID_LENGTH = ('Длина короткой ссылки '
-                               f'до {MAX_SHORT_LENGTH}')
-    FILTERS_REQUIRED = 'Нужно передать хотя бы один фильтр'
-    INVALID_FILTERS = 'Некорректные поля {}'
+    INVALID_SHORT_PATTERN = ('Можно использовать только'
+                             ' латинские буквы и цифры')
+    INVALID_SHORT_LENGTH = ('Длина короткой ссылки '
+                            f'до {MAX_SHORT_LENGTH}')
+    INVALID_ORIGINAL_LENGTH = (f'Не более {ORIGINAL_MAX_LENGTH} '
+                               'символов в ссылке.')
+    GENERATION_FAILED = ('Не получилось сгенерировать короткую ссылку. '
+                         'Попробуйте ещё раз.')
+    INVALID_SHORT = 'Указано недопустимое имя для короткой ссылки'
+    INVALID_ORIGINAL = 'Недопустимый формат url'
+    SHORT_ALREADY_EXISTS = ('Предложенный вариант короткой ссылки '
+                            'уже существует.')
 
 
 class URLMap(db.Model):
@@ -40,29 +50,42 @@ class URLMap(db.Model):
         return {'url': self.original, 'custom_id': self.short}
 
     @staticmethod
-    def create(original, short):
-        db.session.add(URLMap(original=original, short=short))
+    def create(original: str, short: Union[str, None] = None):
+        short = URLMap.get_or_generate_short(short)
+        parsed = urlparse(original)
+        if not all([parsed.scheme, parsed.netloc]):
+            raise ValueError(Message.INVALID_ORIGINAL)
+        entry = URLMap(original=original, short=short)
+        db.session.add(entry)
         db.session.commit()
+        return entry
 
     @staticmethod
-    def from_short(short):
+    def from_short(short: str) -> Union[str, None]:
         entry = URLMap.query.filter_by(short=short).first()
         if entry:
             return entry.original
 
     @staticmethod
-    def generate_short():
-        retry = 1
-        while retry <= MAX_GENERATE_SHORT_RETRIES:
+    def get_or_generate_short(short: Union[str, None] = None):
+        if short:
+            if len(short) > MAX_SHORT_LENGTH:
+                raise ValueError(Message.INVALID_SHORT_LENGTH)
+            if not re.fullmatch(SHORT_PATTERN, short):
+                raise ValueError(Message.INVALID_SHORT)
+            if not URLMap.query.filter_by(short=short).first():
+                return short
+            raise ValueError(Message.SHORT_ALREADY_EXISTS.format(short))
+        for _ in range(MAX_GENERATE_SHORT_RETRIES):
             short = ''.join(
                 choices(POSSIBLE_CHARACTERS, k=RANDOM_SHORT_LENGTH)
             )
             if not URLMap.query.filter_by(short=short).first():
                 return short
-            retry += 1
+        raise ValueError(Message.GENERATION_FAILED)
 
     @staticmethod
-    def get_url_for_short(short):
+    def get_url_for_short(short: str) -> Union[str, None]:
         entry = URLMap.query.filter_by(short=short).first()
         if entry:
             return url_for(REDIRECT_VIEW, short=short, _external=True)
