fc1c12d5b063b1ae0f354d9a02a5fe12a8d2a7a9
Frank Stotch <ya.inside38@gmail.com>
2025-01-31 01:56:31 +0500

    delete converters

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -2,13 +2,10 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from .converters import ShortIDConverter
 from .settings import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-app.url_map.converters['short_id'] = ShortIDConverter
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
