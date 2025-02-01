a48b79c6aa94adc112d4bebd50413e0d4d0ce47b
Frank Stotch <ya.inside38@gmail.com>
2025-02-01 23:25:18 +0500

    fix forms

--- yacut/forms.py
+++ yacut/forms.py
@@ -2,33 +2,45 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
 from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
 
-from .models import Message, ORIGINAL_MAX_LENGTH
-from .settings import MAX_SHORT_LENGTH, SHORT_PATTERN
+
+from .settings import (
+    MAX_SHORT_LENGTH,
+    ORIGINAL_MAX_LENGTH,
+    SHORT_PATTERN
+)
 
 
-class Label:
-    LONG_URL = 'Длинная ссылка'
-    CUSTOM_SHORT_ID = 'Ваш вариант короткой ссылки'
-    SUBMIT = 'Создать'
+REQUIRED_FIELD = 'Обязательное поле'
+INVALID_URL_PATTERN = 'Неправильный формат ссылки'
+INVALID_SHORT_PATTERN = ('Можно использовать только'
+                         ' латинские буквы и цифры')
+INVALID_SHORT_LENGTH = ('Длина короткой ссылки '
+                        f'до {MAX_SHORT_LENGTH}')
+INVALID_ORIGINAL_LENGTH = (f'Не более {ORIGINAL_MAX_LENGTH} '
+                           'символов в ссылке.')
+
+LONG_URL = 'Длинная ссылка'
+CUSTOM_SHORT_ID = 'Ваш вариант короткой ссылки'
+SUBMIT = 'Создать'
 
 
 class URLMapForm(FlaskForm):
     original_link = URLField(
-        label=Label.LONG_URL,
+        label=LONG_URL,
         validators=[
-            DataRequired(Message.REQUIRED_FIELD),
-            URL(message=Message.INVALID_URL_PATTERN),
+            DataRequired(REQUIRED_FIELD),
+            URL(message=INVALID_URL_PATTERN),
             Length(max=ORIGINAL_MAX_LENGTH,
-                   message=Message.INVALID_ORIGINAL_LENGTH)
+                   message=INVALID_ORIGINAL_LENGTH)
         ]
     )
     custom_id = StringField(
-        label=Label.CUSTOM_SHORT_ID,
+        label=CUSTOM_SHORT_ID,
         validators=[
             Optional(),
-            Regexp(SHORT_PATTERN, message=Message.INVALID_SHORT_PATTERN),
+            Regexp(SHORT_PATTERN, message=INVALID_SHORT_PATTERN),
             Length(max=MAX_SHORT_LENGTH,
-                   message=Message.INVALID_SHORT_LENGTH)
+                   message=INVALID_SHORT_LENGTH)
         ]
     )
-    submit = SubmitField(label=Label.SUBMIT)
+    submit = SubmitField(label=SUBMIT)
