f902ac78da54e4aefa95ada5012a2909e2b7d4a7
Frank Stotch <ya.inside38@gmail.com>
2025-02-01 23:25:06 +0500

    fix api_views

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -7,17 +7,17 @@ from .error_handlers import InvalidAPIUsage
 from .models import URLMap
 
 
-ENTRY_DOES_NOT_EXIST = 'Указанный id не найден'
+ID_NOT_EXIST = 'Указанный id не найден'
 NO_REQUEST_BODY = 'Отсутствует тело запроса'
 ORIGINAL_MISSING = '"url" является обязательным полем!'
 
 
 @app.route('/api/id/<string:short>/', methods=['GET'])
 def get_url(short):
-    original = URLMap.from_short(short)
-    if not original:
-        raise InvalidAPIUsage(ENTRY_DOES_NOT_EXIST, HTTPStatus.NOT_FOUND)
-    return jsonify({'url': original}), HTTPStatus.OK
+    entry = URLMap.get(short)
+    if not entry:
+        raise InvalidAPIUsage(ID_NOT_EXIST, HTTPStatus.NOT_FOUND)
+    return jsonify({'url': entry.original}), HTTPStatus.OK
 
 
 @app.route('/api/id/', methods=['POST'])
@@ -25,17 +25,11 @@ def create_id():
     data = request.get_json(silent=True)
     if not data:
         raise InvalidAPIUsage(NO_REQUEST_BODY)
-    original = data.get('url')
-    if not original:
+    if 'url' not in data:
         raise InvalidAPIUsage(ORIGINAL_MISSING)
     try:
-        entry = URLMap.create(original, data.get('custom_id'))
-        return jsonify(
-            {
-                'url': entry.original,
-                'short_link': URLMap.get_url_for_short(entry.short)
-            }
-        ), HTTPStatus.CREATED
+        entry = URLMap.create(data.get('url'), data.get('custom_id'))
+        return jsonify(entry.to_dict()), HTTPStatus.CREATED
     except ValueError as e:
         raise InvalidAPIUsage(str(e))
     except RuntimeError as e:
