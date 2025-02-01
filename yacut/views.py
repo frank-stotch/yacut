d1e1cc3c161a97aa1237426349c25c8f213d8c16
Frank Stotch <ya.inside38@gmail.com>
2025-02-01 23:25:33 +0500

    fix views

--- yacut/views.py
+++ yacut/views.py
@@ -12,21 +12,23 @@ def index_view():
     form = URLMapForm()
     if not form.validate_on_submit():
         return render_template('index.html', form=form)
-    short = form.custom_id.data
     try:
         entry = URLMap.create(original=form.original_link.data,
-                              short=short)
-        return render_template('index.html',
-                               form=form,
-                               short=URLMap.get_url_for_short(entry.short))
-    except ValueError or RuntimeError as e:
+                              short=form.custom_id.data,
+                              full_validation=False)
+        return render_template(
+            'index.html',
+            form=form,
+            short=entry.short
+        )
+    except (ValueError, RuntimeError) as e:
         flash(str(e))
         return render_template('index.html', form=form)
 
 
 @app.route('/<string:short>', methods=['GET'])
 def redirect_view(short: str):
-    original = URLMap.from_short(short=short)
-    if not original:
+    entry = URLMap.get(short=short)
+    if not entry:
         abort(HTTPStatus.NOT_FOUND)
-    return redirect(original)
+    return redirect(entry.original)
